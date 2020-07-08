a6448acebcef10d2026e98375826bbd4aba3cbac
qingxi2020 <kangyong2020@foxmail.com>
2020-07-08 13:17:30 +0800

    test git

--- configuration_files/trajectory_builder_2d.lua
+++ configuration_files/trajectory_builder_2d.lua
@@ -11,7 +11,7 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
-
+-- test git cartographer_ros的默认配置文件在cartographer
 TRAJECTORY_BUILDER_2D = {
   use_imu_data = true,
   min_range = 0.,
